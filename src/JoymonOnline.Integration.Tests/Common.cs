a2062f70e2ce384ef6bbe2e4db431c8e5b0cdd52
joymon <joymon@gmail.com>
2015-10-30 19:58:56 -0400

    Added tests for default title

--- src/JoymonOnline.Integration.Tests/Common.cs
+++ src/JoymonOnline.Integration.Tests/Common.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,49 @@ namespace UI.Tests
     public static class Common
     {
         public const string BaseUrl = "http://localhost/joymononline/";
+        /// <summary>
+        /// This method will check a url to see that it does not return server or protocol errors
+        /// </summary>
+        /// <param name="url">The path to check</param>
+        /// <returns></returns>
+        /// <remarks>http://stackoverflow.com/questions/924679/c-sharp-how-can-i-check-if-a-url-exists-is-valid</remarks>
+        public static bool UrlIsValid(string url)
+        {
+            try
+            {
+                HttpWebRequest request = HttpWebRequest.Create(url) as HttpWebRequest;
+                request.Timeout = 5000; //set the timeout to 5 seconds to keep the user from waiting too long for the page to load
+                request.Method = "HEAD"; //Get only the header information -- no need to download any content
+
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+                int statusCode = (int)response.StatusCode;
+                if (statusCode >= 100 && statusCode < 400) //Good requests
+                {
+                    return true;
+                }
+                else if (statusCode >= 500 && statusCode <= 510) //Server Errors
+                {
+                    Console.WriteLine(String.Format("The remote server has thrown an internal error. Url is not valid: {0}", url));
+                    return false;
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError) //400 errors
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("Unhandled status [{0}] returned for url: {1}", ex.Status, url), ex);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("Could not test url {0}.", url), ex);
+            }
+            return false;
+        }
     }
 }
---
+++ src/JoymonOnline.Integration.Tests/PageTitleTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using FakeHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JoymonOnline.IntegrationTests
+{
+    [TestClass]
+    public class PageTitleTests
+    {
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            //Temp catch to see why this is not working in VSOnline CI
+          
+        }
+     
+    }
+}
