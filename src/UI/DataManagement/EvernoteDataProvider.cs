4b671a656c49665b3913126876ec92226f92de1b
joymon <joymon@gmail.com>
2015-11-13 10:42:54 -0500

    Getting rid of Evennote as data store.

--- src/UI/DataManagement/EvernoteDataProvider.cs
+++ src/UI/DataManagement/EvernoteDataProvider.cs
@@ -1,4 +1,4 @@
-﻿using EvernoteSDK;
+﻿//using EvernoteSDK;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,42 +10,39 @@ namespace Joymononline
     /// <summary>
     /// Evernote data provider. Returns data from joymon@gmail.com evernote account.
     /// </summary>
-    public class EvernoteDataProvider :IWhatsNewDataProvider,IWhoAmIDataProvider
-    {
-        static EvernoteDataProvider()
-        {
-            ENSession.SetSharedSessionDeveloperToken(evernote, noteStoreUrl);
-        }
-        const string evernote= "";
-        const string noteStoreUrl = "https://www.evernote.com/shard/s243/notestore";
-        const string notebookName = "joymononline";
-        string IWhatsNewDataProvider.GetData()
-        {
-            //string whatsNew = GetFromNote("whatsnew");
-            string whatsNew = @"Celebrated Johan's first B'Day.<br/>Blessed with Baby Boy Johan.<br/>Joined <a href='http://www.orioninc.com' target='_blank' data-mce-href='http://www.orioninc.com'>Orion Inc</a> US office as Architect.";
-
-            return whatsNew;
-        }
-        [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Assert, Unrestricted =true )]
-        private static string GetFromNote(string noteName)
-        {
-            if (!ENSession.SharedSession.IsAuthenticated)
-            {
-                ENSession.SharedSession.AuthenticateToEvernote();
-            }
-            IList<ENNotebook> notebooks = ENSession.SharedSession.ListNotebooks();
-            ENNoteSearch search = new ENNoteSearch(noteName);
-            var notes = ENSession.SharedSession.FindNotes(search, null, ENSession.SearchScope.All, ENSession.SortOrder.Normal, 2);
-            ENNote note = ENSession.SharedSession.DownloadNote(notes[0].NoteRef);
-            Console.WriteLine(note.Title);
-            Console.WriteLine(note.TextContent);
-            Console.WriteLine(note.HtmlContent);
-            return note.HtmlContent;
-        }
-
-        string IWhoAmIDataProvider.GetData()
-        {
-            return GetFromNote("whatsnew");
-        }
-    }
+    //public class EvernoteDataProvider :IWhatsNewDataProvider,IWhoAmIDataProvider
+    //{
+    //    static EvernoteDataProvider()
+    //    {
+    //        ENSession.SetSharedSessionDeveloperToken(evernote, noteStoreUrl);
+    //    }
+    //    const string evernote= "";
+    //    const string noteStoreUrl = "https://www.evernote.com/shard/s243/notestore";
+    //    const string notebookName = "joymononline";
+    //    string IWhatsNewDataProvider.GetData()
+    //    {
+    //        string whatsNew = GetFromNote("whatsnew");
+    //        return whatsNew;
+    //    }
+    //    [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Assert, Unrestricted =true )]
+    //    private static string GetFromNote(string noteName)
+    //    {
+    //        if (!ENSession.SharedSession.IsAuthenticated)
+    //        {
+    //            ENSession.SharedSession.AuthenticateToEvernote();
+    //        }
+    //        IList<ENNotebook> notebooks = ENSession.SharedSession.ListNotebooks();
+    //        ENNoteSearch search = new ENNoteSearch(noteName);
+    //        var notes = ENSession.SharedSession.FindNotes(search, null, ENSession.SearchScope.All, ENSession.SortOrder.Normal, 2);
+    //        ENNote note = ENSession.SharedSession.DownloadNote(notes[0].NoteRef);
+    //        Console.WriteLine(note.Title);
+    //        Console.WriteLine(note.TextContent);
+    //        Console.WriteLine(note.HtmlContent);
+    //        return note.HtmlContent;
+    //    }
+    //    string IWhoAmIDataProvider.GetData()
+    //    {
+    //        return GetFromNote("whatsnew");
+    //    }
+    //}
 }
--- src/UI/Evernote/MemoryBasedENPreferenceStore.cs
+++ src/UI/Evernote/MemoryBasedENPreferenceStore.cs
@@ -1,4 +1,4 @@
-﻿using EvernoteSDK.Advanced;
+﻿//using EvernoteSDK.Advanced;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,33 +6,33 @@ using System.Web;
 
 namespace Joymononline.Evernote
 {
-    public class MemoryBasedENPreferenceStore : ENPreferencesStore
-    {
-        private Dictionary<string, object> _Store { get; set; }
-        public MemoryBasedENPreferenceStore()
-        {
-            _Store = new Dictionary<string, object>();
-        }
-        public override object ObjectForKey(string key)
-        {
-            object value = null;
-            _Store.TryGetValue(key, out value);
-            return value;
-        }
-        public override void RemoveAllObjects()
-        {
-            _Store.Clear();
-        }
-        public override void SetObject(object objectToStore, string key)
-        {
-            if (objectToStore != null)
-            {
-                _Store[key] = objectToStore;
-            }
-            else
-            {
-                _Store.Remove(key);
-            }
-        }
-    }
+    //public class MemoryBasedENPreferenceStore : ENPreferencesStore
+    //{
+    //    private Dictionary<string, object> _Store { get; set; }
+    //    public MemoryBasedENPreferenceStore()
+    //    {
+    //        _Store = new Dictionary<string, object>();
+    //    }
+    //    public override object ObjectForKey(string key)
+    //    {
+    //        object value = null;
+    //        _Store.TryGetValue(key, out value);
+    //        return value;
+    //    }
+    //    public override void RemoveAllObjects()
+    //    {
+    //        _Store.Clear();
+    //    }
+    //    public override void SetObject(object objectToStore, string key)
+    //    {
+    //        if (objectToStore != null)
+    //        {
+    //            _Store[key] = objectToStore;
+    //        }
+    //        else
+    //        {
+    //            _Store.Remove(key);
+    //        }
+    //    }
+    //}
 }
