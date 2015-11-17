136931d1815b05336da495f425a0137445ed6153
joymon <joymon@gmail.com>
2015-11-16 22:12:19 -0500

    Added ResourceLoadTests

---
+++ src/JoymonOnline.IntegrationTests/ResourceLoadTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FakeHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JoymonOnline.IntegrationTests
+{
+    [TestClass]
+    public class ResourceLoadTests :IntegrationTest
+    {
+        [TestMethod]
+        public void WhenDefaultCSSIsRequested_ShouldReturn200AndContent()
+        {
+            int expectedSize = 7972;
+            using (var browser = new Browser())
+            {
+                var result = browser.Get("/App_Themes/Black/Default.css");
+                var actualSize = result.ResponseText.Length;
+                Assert.IsTrue(actualSize == expectedSize, "The expected size of {0} is {1}. But actual is {2}", "/App_Themes/Black/Default.css",expectedSize,actualSize);
+            }
+        }
+    }
+}
