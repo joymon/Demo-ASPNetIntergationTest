d4fa8f0d68d960d1294d0f1f06a944de515d4a64
joymon <joymon@gmail.com>
2015-11-30 15:13:42 -0500

    Corrected the title test case of project page

--- src/JoymonOnline.IntegrationTests/JoymonOnline_Projects.cs
+++ src/JoymonOnline.IntegrationTests/JoymonOnline_Projects.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using FakeHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,12 +8,26 @@ namespace JoymonOnline.IntegrationTests
     public class JoymonOnline_Projects : IntegrationTest
     {
         [TestMethod]
+        public void WhenProjectsPageIsRequested_TheResponseShouldHaveTitle()
+        {
+            using (var browser = new Browser())
+            {
+                var result = browser.Get("/Projects.aspx");
+
+                int actual = result.ResponseXml.Descendants("title").Count();
+                Assert.AreEqual(1, actual, "Not able to get the title tag");
+
+            }
+        }
+        [TestMethod]
         public void WhenProjectsPageIsRequested_ShouldReturnWithProperTitle()
         {
             using (var browser = new Browser())
             {
                 var result = browser.Get("/Projects.aspx");
-                Assert.IsTrue(result.ResponseText.Contains("Joymon Online | Projects"), "Projects page title is wrong");
+                string actual = result.ResponseXml.Descendants("title").First().Value.Trim();
+
+                Assert.AreEqual("Joymon Online | Projects",actual, "Projects page title is wrong");
             }
         }
     }
