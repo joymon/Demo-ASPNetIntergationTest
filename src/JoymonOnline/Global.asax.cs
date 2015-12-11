639014d408433afaf2b90d9e4e9954bb6474db60
joymon <joymon@gmail.com>
2015-12-10 20:06:39 -0500

    Fix for wrong error page if there is exception inline tokens of Default.aspx

--- src/JoymonOnline/Global.asax.cs
+++ src/JoymonOnline/Global.asax.cs
@@ -18,6 +18,10 @@ namespace JoymonOnline
         void Application_Error(object sender, EventArgs e)
         {
             // Code that runs when an unhandled error occurs
+            //Clear is really important as Server.Transfer won't clear the things writtne till the exception
+            //TODO - Post into blog.
+            Response.Clear();
+
             Server.Transfer("Error.aspx");
         }
 
