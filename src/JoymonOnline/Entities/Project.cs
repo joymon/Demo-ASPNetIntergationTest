92c7006c117851899cb9d31727b652bae52030ae
joymon <joymon@gmail.com>
2016-01-05 18:10:23 -0500

    Excluded the entity class from code coverage

--- src/JoymonOnline/Entities/Project.cs
+++ src/JoymonOnline/Entities/Project.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Web;
 
 namespace JoymonOnline
 {
+    [ExcludeFromCodeCoverage]
     public class Project
     {
         public string Name { get;set; }
