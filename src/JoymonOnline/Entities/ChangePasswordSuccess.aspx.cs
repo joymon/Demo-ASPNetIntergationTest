2405a5fe994a0be22a50cd62f4471c1789bc37b8
joymon <joymon@gmail.com>
2016-01-05 17:31:49 -0500

    Removed unused code

--- src/JoymonOnline/Entities/ChangePasswordSuccess.aspx.cs
+++ src/JoymonOnline/Entities/ChangePasswordSuccess.aspx.cs
@@ -9,9 +9,5 @@ namespace JoymonOnline
 {
     public partial class ChangePasswordSuccess : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
