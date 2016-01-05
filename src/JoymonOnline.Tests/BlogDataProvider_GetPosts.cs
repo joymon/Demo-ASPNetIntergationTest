2529a1cca4f597d1db9577898364ec3d0008c0eb
joymon <joymon@gmail.com>
2016-01-05 17:50:19 -0500

    Added BlogDataProvider_GetPosts tests

---
+++ src/JoymonOnline.Tests/BlogDataProvider_GetPosts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JoymonOnline.Tests
+{
+    [TestClass]
+    public class BlogDataProvider_GetPosts
+    {
+        [TestMethod]
+        [ExpectedException (typeof(ArgumentNullException))]
+        public void WhenUrlIsEmpty_ThrowException()
+        {
+            BlogDataProvider provider = new BlogDataProvider();
+            IEnumerable<SyndicationItem> items = provider.GetPosts(null, 10);
+        }
+    }
+}
