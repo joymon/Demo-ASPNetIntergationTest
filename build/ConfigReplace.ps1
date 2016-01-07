06c4086ca1d48c8c911dd414daace7f82f2bfb15
joymon <joymon@gmail.com>
2016-01-07 15:08:22 -0500

    Added config replace powershell

---
+++ build/ConfigReplace.ps1
@@ -0,0 +1,15 @@
+﻿"Length of GithubAPIKey is " + $env:GitHubAPIKey.length
+
+$PSScriptRoot
+$appSettingsPath = Join-Path $PSScriptRoot '..\src\*\configs\appSettings.config'
+$appSettingsPath
+#$xml = [xml](Get-Content $appSettingsPath)
+#$node=$xml.appSettings.add | where {$_.key -eq "GitHubAPIToken"}
+#$node.value =$env:GitHubAPIKey
+#$xml.Save($appSettingsPath);
+dir $appSettingsPath -recurse | foreach-object{  
+   $wc = [xml](Get-Content $_.fullname)
+   $wc.SelectNodes("//add[@key='GitHubAPIToken']") | Foreach-Object {$_.value = $env:GitHubAPIKey}
+   "Saved file " + $_.fullname  
+   $wc.Save($_.fullname)  
+}
