b916de5269f536888b1cb2944ba96dc05e8972e2
joymon <joymo@DESKTOP-N7IGI6V>
2017-01-02 20:11:42 -0500

    Serverless - Converted feed from directive to component

--- src/JoymonOnline/JS/app/feed.component.ts
+++ src/JoymonOnline/JS/app/feed.component.ts
@@ -2,39 +2,15 @@
 //Directive for individual Blog Feed
 
 namespace JoymonOnline {
-    export class BlogFeedDirective implements ng.IDirective {
-        public restrict: string;
-        public templateUrl: string;
-        public controller: any;
-        public controllerAs: string;
-        public bindToController: boolean | { [boundProperty: string]: string };
-        public scope: boolean | { [boundProperty: string]: string };
-        constructor() {
-            this.restrict = 'E';
-            this.templateUrl = 'JS/app/feed.component.html';
-            this.controller = "BlogFeedController";
-            this.controllerAs = "ctrl";
-            this.bindToController = true;
-            this.scope = {
-                rss: '@'
-            };
-        }
-    }
-    AppModule.getInstance().registerDirective("blogFeed", () => {
-        console.log("Called reg fn");
-        return new BlogFeedDirective()
-    });
     export class BlogFeedController implements ng.IController {
-
-        //static $inject = ['DataService'];
-        constructor($scope:any,dataServie: BlogDataService) {
+        static $inject = ['DataService'];
+        constructor( dataServie: BlogDataService) {
             console.log("BlogFeedController created");
             this.Url = "joymons world wpf";
             this.Feed = { entries: [] };
             //this.Feed.entries.push({ Title: "Test Title", publishDate: new Date(), content: "test content", link: 'test ur' });
             //this.Feed.entries.push({ Title: "Test Title2", publishDate: new Date(), content: "test content2", link: 'test url2' });
-            //console.log("rss" + $scope.ctrl.rss);
-            dataServie.GetPosts($scope.ctrl.rss)
+            dataServie.GetPosts(this.rss)
                 .then((value: ng.IHttpPromiseCallbackArg<BlogFeedResponse>) => {
                     console.log(value.data.feed);
                     this.Feed = value.data.feed;
@@ -48,7 +24,22 @@ namespace JoymonOnline {
             return this.Url;
         }
     }
-//    AppModule.getInstance().registerController("BlogFeedController", new BlogFeedController());
-    AppModule.getInstance().registerControllerWithFactory("BlogFeedController", ($scope:any, DataService: BlogDataService)=> new BlogFeedController($scope,DataService));
 
+    export class BlogFeedComponent implements ng.IComponentOptions {
+        public templateUrl: string;
+        public controller: any;
+        public controllerAs: string;
+        public bindToController: boolean | { [boundProperty: string]: string };
+        public bindings: { [boundProperty: string]: string };
+        constructor() {
+            this.templateUrl = 'JS/app/feed.component.html';
+            this.controller = BlogFeedController;
+            this.controllerAs = "ctrl";
+            this.bindToController = true;
+            this.bindings = {
+                rss: '@',
+            }
+        }
+    }
+    AppModule.getInstance().registerComponent("blogFeed",new BlogFeedComponent());
 }
