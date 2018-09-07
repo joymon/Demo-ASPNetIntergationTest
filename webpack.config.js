cfab27da041da512e313f16a528a1847d9351300
joymon <joymon@gmail.com>
2018-09-07 15:33:56 -0400

    Upgraded to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,70 @@
 var webpack = require("webpack");
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 module.exports = {
     entry: {
-        app:"./src/JS/app/bootstrap.ts",
+         app:"./src/JS/app/bootstrap.ts",
         vendor:["angular","angular-sanitize","angular-route"]
     },
     output: {
-        filename: "dist/bundle.js"
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist')
     },
     resolve: {
         extensions: [ '.webpack.js', '.web.js', '.ts', '.js']
     },
+
+    optimization:{
+        splitChunks:{
+            name:true,
+            cacheGroups: {
+                vendor: {
+                    test: 'dist/vendor.bundle.js',
+                    name: 'vendor',
+                    chunks: 'initial'
+                }
+            } 
+        }
+    },
     module: {
-        loaders: [
+        rules:[
             { test: /\.ts$/, loader: 'ts-loader' },
             { test: /\.css$/, loaders: ['style-loader', 'css-loader']},
              { test: /\.gif/, loader: "file-loader?emitFile=false&name=App_Themes/Black/images/[name].[ext]" }, 
              {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"}, 
-      {      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"},
-      {      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"    },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"},
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"    },
        {      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"    }, 
        {      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"    }
-        ]
+    ]
     },
     devServer: {
-        contentBase: "dist",
-        hot: true
+        contentBase: "dist"
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({minimize: true}),
-        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'dist/vendor.bundle.js'}),
+        //new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'dist/vendor.bundle.js'}),
         new CopyWebpackPlugin([
             {
                 from: 'src/*.html',
-                to: 'dist',
+                to: '.',
                 flatten:true
             },
             {
                 from:'src/App_Themes/Black/Images',
-                to:"dist/App_Themes/Black/images"
+                to:"App_Themes/Black/images"
             },
             {
                 from:'node_modules/font-awesome/fonts',
-                to:"dist/App_Themes/fonts"
+                to:"App_Themes/fonts"
             },
-            // {
-            //     from:'src/fonts',
-            //     to:"dist/fonts"
-            // },
             {
                 from:'src/JS/app/*.html',
-                to:"dist/JS/app",
+                to:"JS/app",
                 flatten:true
             },
             {
                 from:'src/JS/*.html',
-                to:"dist/JS",
+                to:"JS",
                 flatten:true
             }
         ])
