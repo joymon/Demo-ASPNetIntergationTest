d736c39461fc65275ffdf09855ebdeadc491a3f6
joymon <joymon@gmail.com>
2018-10-17 19:57:00 -0400

    Separated Prayerbook from JoymonOnline hosting

--- src/JS/app/projects.service.ts
+++ src/JS/app/projects.service.ts
@@ -1,38 +1,53 @@
-
 class ProjectsService {
-        static $inject = ["$http", "$q"];
-        private repoUrl = 'https://api.github.com/repos/';
-        private prjs = ["joyful-visualstudio",
-            "Orchestration",
-            "Karel",
-            "PrayerBook"];
-        http: ng.IHttpService;
-        q: ng.IQService;
-        constructor($http: ng.IHttpService, $q: ng.IQService) {
-            this.http = $http;
-            this.q = $q;
-        }
-        private getProjectUrl(userName: string, projectName: string) {
-            return this.repoUrl + userName + "/" + projectName
-        }
-        private logAPIQuota(ret: ng.IHttpPromiseCallbackArg<{}>[]) {
-            if (ret.length > 0) {
-                console.log(ret[0].headers());
-            }
-        }
-        GetAllProjects(userName: string): ng.IHttpPromise<JoymonOnline.ProjectResponse[]> {
-            var deffered = this.q.defer<any>();
-
-            this.q.all(this.prjs.map((value: string, index: number, array: string[]) => {
-                return this.http.get(this.getProjectUrl(userName, value),{cache:true});
-            }))
-                .then((s) => {
-                    this.logAPIQuota(s);
-                    deffered.resolve({ data: s });
-                }, ((err) => { deffered.reject(err); })
-                );
-            return deffered.promise;
-        }
+  static $inject = ["$http", "$q"];
+  private repoUrl = "https://api.github.com/repos/";
+  private prjs = [
+    "dotnet-helpers",
+    "joyful-visualstudio",
+    "Orchestration",
+    "Karel",
+    "PrayerBook"
+  ];
+  http: ng.IHttpService;
+  q: ng.IQService;
+  constructor($http: ng.IHttpService, $q: ng.IQService) {
+    this.http = $http;
+    this.q = $q;
+  }
+  private getProjectUrl(userName: string, projectName: string) {
+    return this.repoUrl + userName + "/" + projectName;
+  }
+  private logAPIQuota(ret: ng.IHttpPromiseCallbackArg<{}>[]) {
+    if (ret.length > 0) {
+      console.log(ret[0].headers());
     }
-    require('./app').JoymonOnline.AppModule.getInstance().registerService("ProjectsService", ProjectsService);
+  }
+  GetAllProjects(
+    userName: string
+  ): ng.IHttpPromise<JoymonOnline.ProjectResponse[]> {
+    var deffered = this.q.defer<any>();
+
+    this.q
+      .all(
+        this.prjs.map((value: string, index: number, array: string[]) => {
+          return this.http.get(this.getProjectUrl(userName, value), {
+            cache: true
+          });
+        })
+      )
+      .then(
+        s => {
+          this.logAPIQuota(s);
+          deffered.resolve({ data: s });
+        },
+        err => {
+          deffered.reject(err);
+        }
+      );
+    return deffered.promise;
+  }
+}
+require("./app")
+  .JoymonOnline.AppModule.getInstance()
+  .registerService("ProjectsService", ProjectsService);
 export = ProjectsService;
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,97 @@
 var webpack = require("webpack");
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 module.exports = {
-    entry: {
-         app:"./src/JS/app/bootstrap.ts",
-        vendor:["angular","angular-sanitize","angular-route"]
-    },
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: [ '.webpack.js', '.web.js', '.ts', '.js']
-    },
+  entry: {
+    app: "./src/JS/app/bootstrap.ts",
+    vendor: ["angular", "angular-sanitize", "angular-route"]
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+  },
 
-    optimization:{
-        splitChunks:{
-            name:true,
-            cacheGroups: {
-                vendor: {
-                    test: 'dist/vendor.bundle.js',
-                    name: 'vendor',
-                    chunks: 'initial'
-                }
-            } 
+  optimization: {
+    splitChunks: {
+      name: true,
+      cacheGroups: {
+        vendor: {
+          test: "dist/vendor.bundle.js",
+          name: "vendor",
+          chunks: "initial"
         }
-    },
-    module: {
-        rules:[
-            { test: /\.ts$/, loader: 'ts-loader' },
-            { test: /\.css$/, loaders: ['style-loader', 'css-loader']},
-             { test: /\.gif/, loader: "file-loader?emitFile=false&name=App_Themes/Black/images/[name].[ext]" }, 
-             {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"}, 
-            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"},
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"    },
-       {      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"    }, 
-       {      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,      loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"    }
+      }
+    }
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: "ts-loader" },
+      { test: /\.css$/, loaders: ["style-loader", "css-loader"] },
+      {
+        test: /\.gif/,
+        loader:
+          "file-loader?emitFile=false&name=App_Themes/Black/images/[name].[ext]"
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?emitFile=false&name=App_Themes/fonts/[name].[ext]"
+      }
     ]
-    },
-    devServer: {
-        contentBase: "dist"
-    },
-    plugins: [
-        //new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'dist/vendor.bundle.js'}),
-        new CopyWebpackPlugin([
-            {
-                from: 'src/*.html',
-                to: '.',
-                flatten:true
-            },
-            {
-                from:'src/App_Themes/Black/Images',
-                to:"App_Themes/Black/images"
-            },
-            {
-                from:'node_modules/font-awesome/fonts',
-                to:"App_Themes/fonts"
-            },
-            {
-                from:'src/JS/app/*.html',
-                to:"JS/app",
-                flatten:true
-            },
-            {
-                from:'src/JS/*.html',
-                to:"JS",
-                flatten:true
-            }
-        ])
-    ]
-}
+  },
+  devServer: {
+    contentBase: "dist"
+  },
+  plugins: [
+    //new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'dist/vendor.bundle.js'}),
+    new CopyWebpackPlugin([
+      {
+        from: "src/*.html",
+        to: ".",
+        flatten: true
+      },
+      {
+        from: "src/App_Themes/Black/Images",
+        to: "App_Themes/Black/images"
+      },
+      {
+        from: "node_modules/font-awesome/fonts",
+        to: "App_Themes/fonts"
+      },
+      {
+        from: "src/JS/app/*.html",
+        to: "JS/app",
+        flatten: true
+      },
+      {
+        from: "src/JS/*.html",
+        to: "JS",
+        flatten: true
+      },
+      {
+        from: "src/apps/prayerbook/*.*",
+        to:'apps/prayerbook',
+        flatten:true
+
+      }
+    ])
+  ]
+};
