0b3fbbd820126c167d1cd955496363b8e7be4f98
Joy George Kunjikkuru <joymon@gmail.com>
2020-02-06 21:41:26 -0500

    Added home-copyright text

--- tests/integration/home.spec.js
+++ tests/integration/home.spec.js
@@ -2,14 +2,18 @@ describe('Home', () => {
     beforeAll(async () => {
         //await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
-    });
-
-    it('should be titled "Joymon Online"', async () => {
-        
         console.log("Navigating to page");
         await page.goto('http://localhost:8080');
         console.log("Navigated to page");
+    });
+
+    it('should be titled "Joymon Online"', async () => {
         await expect(page.title()).resolves.toMatch('Joymon Online');
         //await page.waitFor(1000);
+    });
+    it('should be copyrighted current year"', async () => {
+        const element = await page.$('.footer > .ng-binding');
+        const text= await element.getProperty('textContent');
+        await expect(page.evaluate(element => element.textContent, element)).resolves.toContain('2020');
     });
 });
