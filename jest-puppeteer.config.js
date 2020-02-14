21628c8b3f38a58985581b6017eb74db94057f9c
Joy George Kunjikkuru <joymon@gmail.com>
2020-02-13 20:54:42 -0500

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,6 @@ module.exports = {
     launch: {
         //$env:HEADLESS = "false" #Run from VSCode terminal, to enable browser without modifying this file.
         headless: process.env.HEADLESS !== 'false',
-        //headless: false, //Uncomment for seeing Chrome
         //slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
         devtools: true
     },
