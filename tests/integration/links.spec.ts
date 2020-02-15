24195733d774fa54ffc77cd4e407c70b9d969529
Joy George Kunjikkuru <joymon@gmail.com>
2020-02-15 09:25:11 -0500

    Added links tests

--- tests/integration/links.spec.ts
+++ tests/integration/links.spec.ts
@@ -11,12 +11,21 @@ describe('Home', () => {
         const element = await page.$('[href="#/Links"]');
         await expect(page.evaluate(element => element, element)).resolves.toBeTruthy();
     });
-    it('should navigate to blogs page', async () => {
+    it('should navigate to links page', async () => {
         await expect(page.title()).resolves.toMatch('Joymon Online');
         //await page.waitFor(1000);
         const element = await page.$('[href="#/Links"]');
         await element.click();
         const blogsView = await page.waitForSelector('links');
         await expect(page.evaluate(element => element, blogsView)).resolves.toBeTruthy();
+    });
+    it('should have 4 sections',async ()=>{
+        const elementLength = (await page.$$('#content > dl')).length;
+        expect(elementLength).toBe(4);
+    });
+    it('should have Joymon v/s Code link',async ()=>{
+        // Alternate selector usage #content > dl > dt:nth-of-type(3)
+        const element = await page.$('#content > dl > dt:nth-child(5) > a');
+        await expect(page.evaluate(element=>element.textContent,element)).resolves.toBe("Joymon v/s Code");
     });
 });
